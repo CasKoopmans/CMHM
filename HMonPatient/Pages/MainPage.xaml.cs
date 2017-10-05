b71b772ba4895c022e057106db88fb0f27df97d5
Cas Koopmans <cjwkoopm@avans.nl>
2017-10-05 12:51:05 +0200

    added patient

---
+++ HMonPatient/Pages/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace HMonPatient.Pages
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            HamburberBox.SelectedItem = Home;
+            ContentFrame.Navigate(typeof(HomePage), this);
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            Split.IsPaneOpen = !Split.IsPaneOpen;
+        }
+
+        private void HamburgerBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Home.IsSelected) ContentFrame.Navigate(typeof(HomePage), this);
+            else if (Session.IsSelected) ContentFrame.Navigate(typeof(SessionPage), this);
+        }
+    }
+}
