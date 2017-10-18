4425563b3287be12bb41d9588ec4305318214949
Michel Megens <dev@bietje.net>
2017-10-18 11:35:59 +0200

    add hmondoc client
    Add a client written in WPF.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

---
+++ HMonDoc/LoginWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HMonDoc
+{
+    public partial class LoginWindow : Window, INotifyPropertyChanged
+    {
+        private bool _isloggedin;
+
+        public bool IsLoggedIn
+        {
+            get { return _isloggedin; }
+            set { _isloggedin = value;
+                OnPropertyChanged("IsLoggedIn");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if(handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public LoginWindow()
+        {
+            this.IsLoggedIn = false;
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            TcpClient client;
+            SslStream stream;
+
+            //client = SslConnectionFactory.CreateTcpClient(Hostname.Text, Int32.Parse(Portnumber.Text));
+            //stream = SslConnectionFactory.CreateFromClient(client);
+            //this.IsLoggedIn = true;
+            new MainWindow(null, null).Show();
+            this.Close();
+        }
+    }
+}
---
+++ HMonDoc/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using System.Windows.Media;
+
+namespace HMonDoc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TcpClient client;
+        private SslStream stream;
+
+        public MainWindow(TcpClient client, SslStream stream)
+        {
+            this.client = client;
+            this.stream = stream;
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void CBHeartrate_Unchecked(object sender, RoutedEventArgs e)
+        {  }
+        private void CBHeartrate_Checked(object sender, RoutedEventArgs e)
+        {  }
+
+        private void CBRPM_Unchecked(object sender, RoutedEventArgs e)
+        { }
+        private void CBRPM_Checked(object sender,RoutedEventArgs e)
+        {  }
+
+        private void CBSpeed_Unchecked(object sender, RoutedEventArgs e)
+        { }
+        private void CBSpeed_Checked(object sender, RoutedEventArgs e)
+        { }
+
+        private void CBDistance_Unchecked(object sender, RoutedEventArgs e)
+        {  }
+        private void CBDistance_Checked(object sender, RoutedEventArgs e)
+        {  }
+
+        private void CBResistance_Unchecked(object sender, RoutedEventArgs e)
+        {  }
+        private void CBResistance_Checked(object sender, RoutedEventArgs e)
+        {  }
+
+        private void CBEnergyKJ_Unchecked(object sender, RoutedEventArgs e)
+        {  }
+        private void CBEnergyKJ_Checked(object sender, RoutedEventArgs e)
+        {  }
+
+        private void CBEnergyW_Unchecked(object sender, RoutedEventArgs e)
+        { }
+        private void CBEnergyW_Checked(object sender, RoutedEventArgs e)
+        {  }
+    }
+}
