4f6591073effd841538b056e722c86f43ffd2b54
Cas Koopmans <cjwkoopm@avans.nl>
2017-10-18 13:56:14 +0200

    add graphtool / measurements
    Signed-off-by: Cas Koopmans <cjwkoopm@avans.nl>

--- HMonDoc/MainWindow.xaml.cs
+++ HMonDoc/MainWindow.xaml.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
 using System.Net.Security;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Threading;
 using System.Windows.Media;
+using LiveCharts;
+using LiveCharts.Wpf;
+using System.Collections;
 
 namespace HMonDoc
 {
@@ -20,47 +17,106 @@ namespace HMonDoc
         private TcpClient client;
         private SslStream stream;
 
+        private int[] time1, time2;
+        private ChartValues<double> distance, burned;
+        private ChartValues<int> pulse, rotations, speed, power, reachedpower;
+        string[] tstamps;
+
         public MainWindow(TcpClient client, SslStream stream)
         {
             this.client = client;
             this.stream = stream;
             InitializeComponent();
             this.DataContext = this;
+            getData();
+            Graph();
         }
 
-        private void CBHeartrate_Unchecked(object sender, RoutedEventArgs e)
-        {  }
-        private void CBHeartrate_Checked(object sender, RoutedEventArgs e)
-        {  }
+        private void getData()
+        {
+            time1 = new int[] { 00, 00, 00, 00, 00, 00, 01, 01, 01 };
+            time2 = new int[] { 00, 10, 20, 30, 40, 50, 00, 10, 20 };
+            tstamps = new string[time1.Length];
 
-        private void CBRPM_Unchecked(object sender, RoutedEventArgs e)
-        { }
-        private void CBRPM_Checked(object sender,RoutedEventArgs e)
-        {  }
+            for(int i = 0; i < time1.Length; i++)
+            {
+                SimpleTime time = new SimpleTime(time1[i], time2[i]);
+                tstamps[i] = time.ToString();
+            }
+            
 
-        private void CBSpeed_Unchecked(object sender, RoutedEventArgs e)
-        { }
-        private void CBSpeed_Checked(object sender, RoutedEventArgs e)
-        { }
+            pulse = new ChartValues<int>{ 100, 103, 105, 108, 115, 116, 120, 130, 132 };
+            rotations = new ChartValues<int> { 70, 68, 70, 72, 79, 78, 73, 74, 75 };
+            speed = new ChartValues<int> { 30, 30, 30, 30, 30, 30, 30, 30, 30 };
+            power = new ChartValues<int> { 50, 50, 50, 50, 50, 50, 50, 50, 50 };
+            reachedpower = new ChartValues<int> { 40, 40, 40, 40, 40, 40, 40, 40, 40 };
+            distance = new ChartValues<double> { 10, 20, 30, 40, 50, 60, 70, 80, 90 };
+            burned = new ChartValues<double> { 20, 30, 40, 50, 60, 70, 80, 90, 99 };
+        }
 
-        private void CBDistance_Unchecked(object sender, RoutedEventArgs e)
-        {  }
-        private void CBDistance_Checked(object sender, RoutedEventArgs e)
-        {  }
+        #region Graph stuff
+        private void Graph()
+        {
+            SeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Heartrate (BPM)",
+                    Values = pulse,
+                    PointGeometry = null,
+                    LineSmoothness = 0
+                },
+                new LineSeries
+                {
+                    Title = "RPM",
+                    Values = rotations,
+                    PointGeometry = null,
+                    LineSmoothness = 0
+                },
+                new LineSeries
+                {
+                    Title = "Speed (km/h)",
+                    Values = speed,
+                    PointGeometry = null,
+                    LineSmoothness = 0
+                },
+                new LineSeries
+                {
+                    Title = "Distance (km)",
+                    Values = distance,
+                    PointGeometry = null,
+                    LineSmoothness = 0
+                },
+                new LineSeries
+                {
+                    Title = "Resistance",
+                    Values = power,
+                    PointGeometry = null,
+                    LineSmoothness = 0
+                },
+                new LineSeries
+                {
+                    Title = "Energy (kJ)",
+                    Values = burned,
+                    PointGeometry = null,
+                    LineSmoothness = 0
+                },
+                new LineSeries
+                {
+                    Title = "Energy (W)",
+                    Values = reachedpower,
+                    PointGeometry = null,
+                    LineSmoothness = 0
+                }
+            };
+            
+            Labels = tstamps;
+            DataContext = this;
+        }
 
-        private void CBResistance_Unchecked(object sender, RoutedEventArgs e)
-        {  }
-        private void CBResistance_Checked(object sender, RoutedEventArgs e)
-        {  }
-
-        private void CBEnergyKJ_Unchecked(object sender, RoutedEventArgs e)
-        {  }
-        private void CBEnergyKJ_Checked(object sender, RoutedEventArgs e)
-        {  }
-
-        private void CBEnergyW_Unchecked(object sender, RoutedEventArgs e)
-        { }
-        private void CBEnergyW_Checked(object sender, RoutedEventArgs e)
-        {  }
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
+        #endregion
     }
 }
---
+++ HMonDoc/Measurement.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMonDoc
+{
+    public class Measurement : IComparable<Measurement>
+    {
+        public Measurement(int pulse, int rotations, int speed, int power, double distance, double burned,
+            int time1, int time2, int reachedpower)
+        {
+            Pulse = pulse;
+            Rotations = rotations;
+            Speed = speed;
+            Distance = distance;
+            Power = power;
+            Burned = burned;
+            Time = new SimpleTime(time1, time2);
+            ReachedPower = reachedpower;
+            Message = GetMessageToSend();
+        }
+
+        public int Pulse { get; set; }
+        public int Rotations { get; set; }
+        public int Speed { get; set; }
+        public double Distance { get; set; }
+        public int Power { get; set; }
+        public double Burned { get; set; }
+        public SimpleTime Time { get; set; }
+        public int ReachedPower { get; set; }
+        public dynamic Message { get; set; }
+
+        public int CompareTo(Measurement other)
+        {
+            if (Time == other.Time)
+                return 0;
+            if (Time > other.Time)
+                return 1;
+            if (Time < other.Time)
+                return -1;
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return $"Pulse : {Pulse} bpm" +
+                   $"\nRotations : {Rotations} RPM" +
+                   $"\nSpeed : {Speed} km/h" +
+                   $"\nPower : {Power} Watt" +
+                   $"\nDistance : {Distance:##.00} KM" +
+                   $"\nBurned : {Burned:##.00} KJoule" +
+                   $"\nTime : {Time}" +
+                   $"\nReachedPower : {ReachedPower} Watt" +
+                   $"\n-----------------------------------\n";
+        }
+
+        public string BackSlashNToString()
+        {
+            return $"Time : {Time}\\n" +
+                   $"\\nSpeed : {Speed}" +
+                   $"\\nRotations : {Rotations}" +
+                   $"\\nPulse : {Pulse}\\n" +
+                   $"\\nDistance : {Distance:##.00}" +
+                   $"\\nBurned : {Burned:##.00}\\n" +
+                   $"\\nPower : {Power}" +
+                   $"\\nReachedPower : {ReachedPower}";
+        }
+
+        private dynamic GetMessageToSend()
+        {
+            dynamic toSend = new
+            {
+                id = "measurement/add",
+                data = new
+                {
+                    pulse = Pulse,
+                    rotations = Rotations,
+                    speed = Speed,
+                    distance = Distance,
+                    power = Power,
+                    burned = Burned,
+                    time = Time,
+                    reachedpower = ReachedPower
+                }
+            };
+            return toSend;
+        }
+    }
+
+
+    public struct SimpleTime
+    {
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is SimpleTime && Equals((SimpleTime)obj);
+        }
+
+        public bool Equals(SimpleTime other)
+        {
+            return (Minutes == other.Minutes) && (Seconds == other.Seconds);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Minutes * 397) ^ Seconds;
+            }
+        }
+
+        public readonly int Minutes;
+        public readonly int Seconds;
+
+        public SimpleTime(int min, int sec)
+        {
+            Minutes = min;
+            Seconds = sec;
+        }
+
+        public override string ToString()
+        {
+            return $"{Minutes:00}:{Seconds:00}";
+        }
+
+        public static bool operator >(SimpleTime first, SimpleTime second)
+        {
+            if (first.Minutes > second.Minutes)
+                return true;
+            if (first.Minutes == second.Minutes)
+                return first.Seconds > second.Seconds ? true : false;
+            return false;
+        }
+
+        public static bool operator <(SimpleTime first, SimpleTime second)
+        {
+            if (first.Minutes < second.Minutes)
+                return true;
+            if (first.Minutes == second.Minutes)
+                return first.Seconds < second.Seconds ? true : false;
+            return false;
+        }
+
+        public static bool operator ==(SimpleTime first, SimpleTime second)
+        {
+            return (first.Minutes == second.Minutes) && (first.Seconds == second.Seconds);
+        }
+
+        public static bool operator !=(SimpleTime first, SimpleTime second)
+        {
+            return (first.Minutes != second.Minutes) || (first.Seconds != second.Seconds);
+        }
+    }
+}
